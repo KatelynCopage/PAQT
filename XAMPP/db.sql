fd09bf2c1b3902197aafeaeda6f09963e8ca2797
VnRice <93795698+VnRice@users.noreply.github.com>
2022-04-29 15:29:33 -0600

    Update db.sql

--- XAMPP/db.sql
+++ XAMPP/db.sql
@@ -2,7 +2,7 @@
 --
 -- Database: `db_connect`
 --
-
+-- Creates a database if not already created 
 CREATE DATABASE IF NOT EXISTS `db_connect` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `db_connect`;
 
@@ -11,25 +11,10 @@ USE `db_connect`;
 --
 -- Table structure for table `tbl_contact`
 --
-
+--creates the tables 
 DROP TABLE IF EXISTS `tbl_contact`;
 CREATE TABLE `tbl_contact` (
   `email` varchar(100) NOT NULL,
   `password` varchar(50) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tbl_contact`
---
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbl_contact`
---
