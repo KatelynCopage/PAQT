1b8b1aaf1412d7e4ce53a6ff3021b40282eee75e
VnRice <93795698+VnRice@users.noreply.github.com>
2022-04-29 15:24:34 -0600

    Update connect.php

--- XAMPP/connect.php
+++ XAMPP/connect.php
@@ -1,24 +1,28 @@
+
 <?php
 
 //Connects localhost to phpMyAdmin and the database called db_connect
 $con = mysqli_connect('localhost', 'root', '','db_connect');
 
-// get email and password 
 
 $txtEmail = $_POST['txtEmail'];
 $txtPass = $_POST['txtPass'];
 
-//insert into sql database 
+//mitigations 
+$txtEmail = mysqli_real_escape_string($_POST['txtEmail']);
+$txtPass = mysqli_real_escape_string($_POST['txtPass']);
 
-$sql = "INSERT INTO `tbl_contact` (`email`, `password`) VALUES ('$txtEmail', '$txtPass')";
+//Insert SQL string into database 
+$sql = "SELECT * FROM tbl_contact WHERE email = '".$txtEmail . "' AND password = '".$txtPass."';";
 
+//Goes to homepage if returns true otherwise returns an error
 $rs = mysqli_query($con, $sql);
-
-
-// redirecting to test.html once connected to php
-if($rs)
-{
+if(mysqli_num_rows($rs) == 1) {
 	header('Location: test.html');
+} else{
+	print_r("Login Unsuccessful");
 }
+
+
 
 ?>
